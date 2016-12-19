2325236c5632da7851b172f53e425b82336ca3eb
sofiekhullar <sofie_k@live.se>
2016-12-19 12:10:42 +0900

    info boxed

--- AccountRegister/app/src/main/java/sofie/accountregister/Step1.java
+++ AccountRegister/app/src/main/java/sofie/accountregister/Step1.java
@@ -1,11 +1,20 @@
 package sofie.accountregister;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
 /**
@@ -15,6 +24,7 @@ public class Step1 extends AppCompatActivity {
 
     // Step1
     Button step1ButtonCreate, step1ButtonCancel;
+    ImageButton step1InfoButtonName, step1InfoButtonEmail;
     EditText step1EditTextName,step1EditTextEmail,step1EditTextDD, step1EditTextMM, step1EditTextYYYY;
     Spinner step1SpinnerGender;
 
@@ -26,12 +36,29 @@ public class Step1 extends AppCompatActivity {
         // Step1
         step1ButtonCreate = (Button) findViewById(R.id.step1ButtonCreate);
         step1ButtonCancel = (Button) findViewById(R.id.step1ButtonCancel);
+        step1InfoButtonName = (ImageButton) findViewById(R.id.step1InfoButtonName);
+        step1InfoButtonEmail = (ImageButton) findViewById(R.id.step1InfoButtonEmail);
         step1EditTextName = (EditText) findViewById(R.id.step1EditTextName);
         step1EditTextEmail = (EditText) findViewById(R.id.step1EditTextEmail);
         step1EditTextDD = (EditText) findViewById(R.id.step1EditTextDD);
         step1EditTextMM = (EditText) findViewById(R.id.step1EditTextMM);
         step1EditTextYYYY = (EditText) findViewById(R.id.step1EditTextNameYYYY);
         step1SpinnerGender = (Spinner) findViewById(R.id.Step1SpinnerGender);
+
+
+        step1EditTextName.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {}
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //step1InfoButtonName.tint
+            }
+        });
+
 
         step1ButtonCreate.setOnClickListener(new View.OnClickListener() {
             @Override
