388d35649ec0a053de944da9ab0972b95e74610f
sofiekhullar <sofie_k@live.se>
2016-12-19 10:47:00 +0900

    Names in xml and manifest fix

--- AccountRegister/app/src/main/java/sofie/accountregister/LoginScreen.java
+++ AccountRegister/app/src/main/java/sofie/accountregister/LoginScreen.java
@@ -2,19 +2,25 @@ package sofie.accountregister;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class LoginScreen extends AppCompatActivity {
 
-    Button loginButton, registerButton;
+    Button loginButton, loginRegisterButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         loginButton = (Button) findViewById(R.id.loginButton);
-        registerButton = (Button) findViewById(R.id.createButton);
+        loginRegisterButton = (Button) findViewById(R.id.createButton);
 
-
+        loginRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_step1);
+            }
+        });
     }
 }
